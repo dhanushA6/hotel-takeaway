c08a1e1e0c933c8a88f102914ff38ccb38aed0dd
henry-jacq <henryeditz267@gmail.com>
2024-01-06 17:07:11 +0530

    Minor changes

--- readwrite_pkl.py
+++ readwrite_pkl.py
@@ -1,10 +1,10 @@
+import pickle
+import wrapper as wp
 from model.menu_manage import *
 from model.order_manage import *
-import pickle
-import wrapper as wp 
 
-def make_assigment():
-     
+
+def make_assignment():
     menu_manager = MenuManager()
     breakfast_menu = menu_manager.get_menu("Breakfast")
     dinner_menu = menu_manager.get_menu("Dinner")
@@ -32,7 +32,6 @@ def make_assigment():
     snack_menu.items = menus["Snacks"]
 
 def make_menu_empty():
-     
     menu_manager = MenuManager()
     breakfast_menu = menu_manager.get_menu("Breakfast")
     dinner_menu = menu_manager.get_menu("Dinner")
@@ -59,11 +58,11 @@ def make_menu_empty():
     snack_menu = menu_manager.get_menu("Snacks")
     snack_menu.items = []
 
-    print("Menus are got  Empty")
+    print("Menus are got empty!")
 
 if __name__ == "__main__":
     try:
-        with open('menu.pickle', 'rb') as f:
+        with open(wp.getPickle('menu'), 'rb') as f:
             menus = pickle.load(f)
             print("menus data Loaded Successfully")
     except:
@@ -82,22 +81,17 @@ if __name__ == "__main__":
         "Dinner": dinner_menu.items,
         "Dessert": dessert_menu.items }
     
-    make_assigment()
+    make_assignment()
 
     menu_manager = MenuManager()
     breakfast_menu = menu_manager.get_menu("Breakfast")
-    print(breakfast_menu.items)
     dinner_menu = menu_manager.get_menu("Dinner")
     lunch_menu = menu_manager.get_menu("Lunch")
     today_menu = menu_manager.get_menu("TodayMenu")
     dessert_menu = menu_manager.get_menu("Dessert")
     snack_menu = menu_manager.get_menu("Snacks")
-    
-   
-  
 
-    # # # Adding items to Breakfast menu
-   
+    # Adding items to Breakfast menu
     # breakfast_menu = menu_manager.get_menu("Breakfast")
     # breakfast_menu.add_item("Poori", 30, "Deep-fried Indian bread, made from unleavened wheat dough, served fluffy and puffed.", "poori.jpg", "Breakfast")
     # breakfast_menu.add_item("Idly", 8, "Steamed, spongy rice and urad dal dumplings, a staple in South Indian cuisine", "Idly.jpg", "Breakfast")
@@ -108,7 +102,6 @@ if __name__ == "__main__":
     # breakfast_menu.add_item("Aloo Parota", 105, "Aloo Parota, An Indian flatbread with spiced mashed potatoes, pan-fried golden, served with yogurt or pickles.", "aloo-parota.jpg", "Breakfast")
     # breakfast_menu.add_item("Double Omelette", 30, "Double Omelette, a protein-packed breakfast, features two perfectly cooked layers, served with toast for a delicious start.", "omelette.jpg", "Breakfast")
     
-
     # # Adding items to Lunch menu
     # lunch_menu = menu_manager.get_menu("Lunch")
     # lunch_menu.add_item("Chicken Biriyani", 150, "A flavorful Indian rice dish with aromatic basmati rice, chicken, and a blend of spices, cooked to perfection.", "chicken-biriyani.png", "Lunch")
@@ -119,8 +112,6 @@ if __name__ == "__main__":
     # lunch_menu.add_item("Veg Fried Rice", 80, "Stir-fried rice with mixed vegetables, often seasoned with soy sauce and spices, creating a flavorful and quick vegetarian dish.", "veg-fried-rice.jpg", "Lunch")
     #lunch_menu.add_item("Curd Rice", 30, "A simple South Indian dish combining rice with yogurt, often tempered with mustard seeds, curry leaves, and green chillies.", "curd-rice.jpg", "Lunch")
     #lunch_menu.add_item("Full Meals", 80, "A balanced Indian meal: rice, chapati, curry, dal, veggies, and dessert - a wholesome dining experience in one plate.", "meals.jpg", "Lunch")
-    
-    wp.remove_item_from_menu("pizza", "Lunch")
     #wp.add_item_to_menu("Full Meals", "Lunch", 80, "A ba    lanced Indian meal: rice, chapati, curry, dal, veggies, and dessert - a wholesome dining experience in one plate.","meals.jpg")
    
     menus = {
@@ -133,11 +124,10 @@ if __name__ == "__main__":
    
     }
 
-    
     try:
-        with open('menu.pickle', 'wb') as file:
+        with open(wp.getPickle('menu'), 'wb') as file:
             pickle.dump(menus, file)
             print("Data Written Successfully")
     except Exception as e:
         print(f"Error saving menus: {e}")
-    
+
